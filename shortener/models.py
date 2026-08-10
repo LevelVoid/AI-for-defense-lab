dfb224dfc34934a0620d3350c763975844af3c0d
Pradeep <pradeep211005@gmail.com>
2026-08-10 19:57:22 +0530

    shortener model added

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class ShortURL(models.Model):
+    short_code = models.CharField(
+        max_length=16,
+        unique=True
+    )
+
+    original_url = models.URLField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    expires_at = models.DateTimeField(
+        null=True,
+        blank=True
+    )
+
+    is_active = models.BooleanField(
+        default=True
+    )
+    
